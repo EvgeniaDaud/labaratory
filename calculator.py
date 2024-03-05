0b5945d357b76104c3f695bb88f1f6be1969928e
unknown <xx.eu@yandex.ru>
2024-03-05 21:46:49 +0300

    Добавлено считывание операции, чисел; обработка условий для калькулятора

--- calculator.py
+++ calculator.py
@@ -13,3 +13,26 @@ def multiply(x, y):
 # Функция для деления
 def divide(x, y):
     return x / y
+print("Выберите операцию:")
+print("1. Сложение")
+print("2. Вычитание")
+print("3. Умножение")
+print("4. Деление")
+
+# Запрашиваем у пользователя выбор операции
+choice = input("Введите номер операции (1/2/3/4): ")
+
+# Запрашиваем у пользователя ввести два числа
+num1 = float(input("Введите первое число: "))
+num2 = float(input("Введите второе число: "))
+
+if choice == '1':
+    print("Результат сложения:", add(num1, num2))
+elif choice == '2':
+    print("Результат вычитания:", subtract(num1, num2))
+elif choice == '3':
+    print("Результат умножения:", multiply(num1, num2))
+elif choice == '4':
+    print("Результат деления:", divide(num1, num2))
+else:
+    print("Неверный выбор операции")
