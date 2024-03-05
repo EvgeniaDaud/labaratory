78f5f5c1d97dfe98c8ec50b8916a956c39d7d9fb
unknown <xx.eu@yandex.ru>
2024-03-05 21:12:44 +0300

    Добавлены функции сложения, вычитания, деления, умножения для программы Calculator

---
+++ calculator.py
@@ -0,0 +1,15 @@
+# Функция для сложения
+def add(x, y):
+    return x + y
+
+# Функция для вычитания
+def subtract(x, y):
+    return x - y
+
+# Функция для умножения
+def multiply(x, y):
+    return x * y
+
+# Функция для деления
+def divide(x, y):
+    return x / y
